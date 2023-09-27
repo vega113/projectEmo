0b7449218e8dd30152151636de1b615d1e4c3533
vega <vega113@gmail.com>
2023-09-27 12:09:36 +0300

    Some clean up

--- app/Module.scala
+++ app/Module.scala
@@ -7,8 +7,5 @@ import play.api.libs.concurrent.AkkaGuiceSupport
 class Module extends AbstractModule with AkkaGuiceSupport {
   override def configure(): Unit = {
     bind(classOf[LiquibaseRunner]).asEagerSingleton()
-    bind(classOf[DatabaseExecutionContext]).to(classOf[DatabaseExecutionContextImpl]).asEagerSingleton()
-    bind(classOf[JwtService]).to(classOf[JwtServiceImpl]).asEagerSingleton()
-    bind(classOf[JwtService]).to(classOf[JwtServiceImpl]).asEagerSingleton()
   }
 }
--- app/dao/DatabaseExecutionContext.scala
+++ app/dao/DatabaseExecutionContext.scala
@@ -2,11 +2,12 @@ package dao
 
 import java.sql.Connection
 import javax.inject.Inject
-
 import akka.actor.ActorSystem
+import com.google.inject.ImplementedBy
 import play.api.db.Database
 import play.api.libs.concurrent.CustomExecutionContext
 
+@ImplementedBy(classOf[DatabaseExecutionContextImpl])
 trait DatabaseExecutionContext {
   def withConnection[A](block: Connection => A): A
 }
