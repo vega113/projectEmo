8aa9d2f29dbc4a8ae426685814254f55aabe5da1
vega <vega113@gmail.com>
2023-10-28 08:42:56 +0300

    Hides footer on smaller devices.

--- ui/src/app/main-layout/main-layout.component.ts
+++ ui/src/app/main-layout/main-layout.component.ts
@@ -4,6 +4,7 @@ import {AuthService} from "../services/auth.service";
 import {MatSidenav} from "@angular/material/sidenav";
 import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {Router} from "@angular/router";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-main-layout',
@@ -12,6 +13,7 @@ import {Router} from "@angular/router";
 })
 export class MainLayoutComponent {
   @ViewChild('sidenav', { static: false }) sidenav!: MatSidenav;
+  @ViewChild('footer', { static: false }) footer!: MatToolbar;
   public isAuthenticated!: Observable<boolean>;
   screenWidth!: number;
   resizeSubscription!: Subscription;
@@ -23,20 +25,26 @@ export class MainLayoutComponent {
 
   ngOnInit(): void {
     this.resizeSubscription = this.breakpointObserver
-      .observe([Breakpoints.Small, Breakpoints.HandsetPortrait])
-      .subscribe((state) => {
-        if (state.matches) {
-          this.screenWidth = 0;
-          if (this.sidenav != null) {
-            this.sidenav.close();
+        .observe([Breakpoints.Small, Breakpoints.HandsetPortrait])
+        .subscribe((state) => {
+          if (state.matches) {
+            this.screenWidth = 0;
+            if (this.sidenav != null) {
+              this.sidenav.close();
+            }
+            if (this.footer != null) {
+              this.footer._elementRef.nativeElement.style.display = 'none';
+            }
+          } else {
+            this.screenWidth = window.innerWidth;
+            if (this.sidenav != null) {
+              this.sidenav.open();
+            }
+            if (this.footer != null) {
+              this.footer._elementRef.nativeElement.style.display = 'block';
+            }
           }
-        } else {
-          this.screenWidth = window.innerWidth;
-          if (this.sidenav != null) {
-            this.sidenav.open();
-          }
-        }
-      });
+        });
   }
 
   ngOnDestroy(): void {
