0a418cdf8b51c44a6345b198534d76de69f1eaef
vega <vega113@gmail.com>
2023-11-12 19:41:13 +0200

    Fixes bug that caused empty sub emotions.

--- app/controllers/model.scala
+++ app/controllers/model.scala
@@ -6,13 +6,12 @@ import play.api.libs.json.{Json, OFormat}
 object model {
   case class EmotionData(emotionTypes: List[EmotionTypesWithEmotions], triggers: List[Trigger])
   case class EmotionTypesWithEmotions(emotionType: String, emotions: List[EmotionWithSubEmotions])
-  case class EmotionWithSubEmotions(emotion: Emotion, subEmotions: List[SubEmotionWithActions])
-  case class SubEmotionWithActions(subEmotion: SubEmotion, suggestedActions: List[SuggestedAction])
+  case class EmotionWithSubEmotions(emotion: Emotion, subEmotions: List[SubEmotion])
+
   case class TagData(tagName: String, emotionRecordId: Long)
 
   object EmotionData {
     implicit val tagDataFormat: OFormat[TagData] = Json.format[TagData]
-    implicit val subEmotionActionFormat: OFormat[SubEmotionWithActions] = Json.format[SubEmotionWithActions]
     implicit val emotionSubEmotionFormat: OFormat[EmotionWithSubEmotions] = Json.format[EmotionWithSubEmotions]
     implicit val emotionTypesWithEmotionsFormat: OFormat[EmotionTypesWithEmotions] = Json.format[EmotionTypesWithEmotions]
     implicit val emotionDataFormat: OFormat[EmotionData] = Json.format[EmotionData]
--- app/service/EmotionDataService.scala
+++ app/service/EmotionDataService.scala
@@ -2,6 +2,7 @@ package service
 
 import com.google.inject.ImplementedBy
 import controllers.model._
+import dao.model.{Emotion, SubEmotion}
 import dao.{DatabaseExecutionContext, EmotionDao, SubEmotionDao, SuggestedActionDao, TriggerDao}
 
 import javax.inject.Inject
@@ -15,15 +16,19 @@ trait EmotionDataService {
 
 class EmotionDataServiceImpl @Inject()(emotionDao: EmotionDao,
                                    triggerDao: TriggerDao,
-                                   databaseExecutionContext: DatabaseExecutionContext
+                                   databaseExecutionContext: DatabaseExecutionContext,
+                                    subEmotionDao: SubEmotionDao,
                                   ) extends EmotionDataService {
   override def fetchEmotionData(): Future[EmotionData] = { // TODO: add caching
     Future {
       databaseExecutionContext.withConnection { implicit connection =>
-        val emotions = emotionDao.findAll()
+        val emotions: List[Emotion] = emotionDao.findAll()
         val triggers = triggerDao.findAll()
+
+        val subEmotions: List[SubEmotion] = subEmotionDao.findAll()
+
         val emotionSubEmotions: List[EmotionWithSubEmotions] = emotions.map(emotion =>
-          controllers.model.EmotionWithSubEmotions(emotion, List()))
+          controllers.model.EmotionWithSubEmotions(emotion, subEmotions.filter(_.parentEmotionId == emotion.id)))
         val emotionTypes = emotionSubEmotions.groupBy(_.emotion.emotionType).map {
           case (Some(emotionType), emotionWithSubEmotions) => EmotionTypesWithEmotions(emotionType,
             emotionWithSubEmotions)
