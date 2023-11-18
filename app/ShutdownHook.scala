2351296ebfdc59079dd89772003e41760ec2264f
vega <vega113@gmail.com>
2023-11-18 20:22:50 +0200

    Updates to print thread dump only if appConfig enabled

--- app/ShutdownHook.scala
+++ app/ShutdownHook.scala
@@ -1,15 +1,15 @@
 import org.slf4j.Logger
+import play.api.Configuration
 
 import scala.concurrent.Future
 import play.api.inject.ApplicationLifecycle
 
 import javax.inject._
 
-class ShutdownHook @Inject()(lifecycle: ApplicationLifecycle) {
+class ShutdownHook @Inject()(lifecycle: ApplicationLifecycle, config: Configuration) {
   val logger: Logger = org.slf4j.LoggerFactory.getLogger("ShutdownHook")
 
-  lifecycle.addStopHook { () =>
-    logger.info("ShutdownHook is running")
+  private def threadDump(): Unit = {
     val threadMXBean = java.lang.management.ManagementFactory.getThreadMXBean
     val threadInfos = threadMXBean.getThreadInfo(threadMXBean.getAllThreadIds, 100)
     val stackTrace = threadInfos.map(threadInfo => {
@@ -19,6 +19,13 @@ class ShutdownHook @Inject()(lifecycle: ApplicationLifecycle) {
       """
     }).mkString("\n")
     logger.info(s"Thread dump:\n$stackTrace")
+  }
+
+  lifecycle.addStopHook { () =>
+    logger.info("ShutdownHook is running")
+    if (config.get[Boolean]("emo.threadDumpOnShutdown")) {
+      threadDump()
+    }
     Future.successful(())
   }
 }
