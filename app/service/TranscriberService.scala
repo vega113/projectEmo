7c59b6baa90afaf72c386f20d858e686f3df7723
Yuri Z <vega113@gmail.com>
2024-06-19 21:05:35 +0300

    Add audio transcription feature using OpenAI for emotion notes (#21)
    - Add `TranscriberController` for handling audio file uploads
    - Implement `TranscriberService` with OpenAI Whisper integration
    - Create model `TranscribedText` for the transcribed text response
    - Update `Module.scala` for dependency injection configuration
    - Add routes for transcribing audio files
    - Integrate media recorder in note form component for recording and uploading audio
    - Update frontend models and services to handle transcription response
    - Add OpenAI dependency to `build.sbt`

---
+++ app/service/TranscriberService.scala
@@ -0,0 +1,50 @@
+package service
+
+import com.google.inject.ImplementedBy
+import dao.model.TranscribedText
+import io.github.sashirestela.openai.SimpleOpenAI
+import io.github.sashirestela.openai.domain.audio.TranscriptionRequest.TimestampGranularity
+import io.github.sashirestela.openai.domain.audio.{AudioResponseFormat, Transcription, TranscriptionRequest}
+import play.api.Configuration
+
+import java.nio.file.{Path, Paths}
+import javax.inject.{Inject, Named}
+import util.RichCompletableFuture._
+
+import java.net.http.HttpClient
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+
+@ImplementedBy(classOf[OpenAiWhisperServiceImpl])
+trait TranscriberService {
+  def transcribeAudioToText(path: Path): Future[TranscribedText]
+}
+
+class OpenAiWhisperServiceImpl @Inject() (config: Configuration) extends TranscriberService {
+
+  private val executionContext: ExecutionContext = ExecutionContext.global
+  private val executorService = executionContext.asInstanceOf[java.util.concurrent.ExecutorService]
+
+  override def transcribeAudioToText(path: Path): Future[TranscribedText] = {
+
+    val duration = java.time.Duration.ofSeconds(config.get[Duration]("openai.timeout").toSeconds.toInt)
+    val httpClient = HttpClient.newBuilder()
+      .connectTimeout(duration)
+      .executor(executorService)
+      .build()
+    val openAi = SimpleOpenAI.builder()
+      .apiKey(config.get[String]("openai.apikey"))
+      .httpClient(httpClient)
+      .build()
+
+
+    val audioRequest = TranscriptionRequest.builder.file(path).model("whisper-1").
+      responseFormat(AudioResponseFormat.VERBOSE_JSON).temperature(0.2).timestampGranularity(TimestampGranularity.WORD).
+      timestampGranularity(TimestampGranularity.SEGMENT).build
+
+    openAi.audios.transcribe(audioRequest).asScala.map(response => {
+      TranscribedText(response.getText)
+    })
+  }
+}
