60622c0d083eb2644b018a8c81723ccf9f85c992
vega <vega113@gmail.com>
2024-06-26 07:28:22 +0300

    Small clean up

--- app/Module.scala
+++ app/Module.scala
@@ -1,11 +1,9 @@
 import com.google.inject.AbstractModule
-import com.google.inject.internal.SingletonScope
 import com.google.inject.name.Names.named
 import io.cequence.openaiscala.service.OpenAIService
-import io.github.sashirestela.openai.SimpleOpenAI
 import liquibase.LiquibaseRunner
 import play.api.libs.concurrent.AkkaGuiceSupport
-import providers.{NoNonsenseOpenAIProvider, SimpleOpenAIProvider}
+import providers.NoNonsenseOpenAIProvider
 import service.ai.{ChatGptEmotionDetectionServiceImpl, EmoDetectionServiceWithAssistantImpl, EmotionDetectionService}
 
 import javax.inject.Singleton
@@ -21,8 +19,6 @@ class Module extends AbstractModule with AkkaGuiceSupport {
 
     bind(classOf[EmotionDetectionService]).annotatedWith(named("ChatGptAssistant")).
       to(classOf[EmoDetectionServiceWithAssistantImpl]).in(classOf[Singleton])
-
-//    bind(classOf[SimpleOpenAI]).toProvider(classOf[SimpleOpenAIProvider])
 
     bind(classOf[OpenAIService]).toProvider(classOf[NoNonsenseOpenAIProvider]).in(classOf[Singleton])
   }
